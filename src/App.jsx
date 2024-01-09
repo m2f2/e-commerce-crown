96924290d8990956af0c4a365ced3faf85c170fc
m2f2 <fayedmostafa5@gmail.com>
2024-01-09 12:27:25 +0200

    updating our directory page with directory component and category item component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./categories.styles.scss";
+import Directory from "./components/directory/directory.component";
+function App() {
+  
+
+  return (
+   <>
+    <Directory/>
+   </>
+  )
+}
+
+export default App
