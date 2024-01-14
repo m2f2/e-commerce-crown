3e68721679b41ab7a35b0c30eb45d9c6fa052d6e
m2f2 <fayedmostafa5@gmail.com>
2024-01-14 09:30:56 +0200

    created checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,22 @@
-import "./cart-dropdown.style.scss";
-import Button from "../button/button.component";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../../contexts/cart.context";
 
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+import "./cart-dropdown.style.scss";
 const CartDropDown = () =>{
+          const {cartItems} = useContext(CartContext);
+          const navigation = useNavigate();
+
+          const goToCheckoutHandler = ()=> navigation('/checkout');
           return(
                     <div className="cart-dropdown-container">
-                              <div className="cart-items"/>
-                              <Button>GO TO CHECKOUT</Button>
-                    </div>
+                              <div className="cart-items">
+                                        {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
+                              </div>
+                              <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+                    </div> 
           )
 }
 
